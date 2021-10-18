d37aa125913f8d660c968dcb85aad21365006633
Hon <dennis.mejen@gmail.com>
2021-10-18 23:45:26 +0700

    [Added] 2 - The useQuery Hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import * as React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
 
+const queryClient = new QueryClient();
+
 function App() {
   const [page, setPage] = React.useState("planets");
 
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
